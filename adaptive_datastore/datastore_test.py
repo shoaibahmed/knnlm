fcf8a2235da4fdc171c294a07c228e0f317353f7
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2023-07-11 10:04:47 +0200

    train function added

--- adaptive_datastore/datastore_test.py
+++ adaptive_datastore/datastore_test.py
@@ -123,7 +123,22 @@ class kNN_LM:
 
         # dataset should give both extra context (from the previous sequence and the actual sequence itself)
         for extra_context, sequence in dataset:
-            output, last_layer_ffn_input = self.model(x)
+            # TODO: Convert to a batched implementation
+            full_input = extra_context + sequence
+
+            extra_context_input_ids = self.tokenizer(extra_context, return_tensors="pt")["input_ids"]
+            full_input_tok = self.tokenizer(full_input, return_tensors="pt")
+
+            output, last_layer_ffn_input = self.model(**full_input_tok)
+            selected_last_layer_ffn_input = last_layer_ffn_input[:, extra_context_input_ids-1:, :]
+            assert selected_last_layer_ffn_input.shape[1] == len(sequence), selected_last_layer_ffn_input.shape
+
+            # Convert the whole sequence into set of target words
+            sequence_tokenized = full_input_tok["input_ids"][torch.arange(), extra_context_input_ids.shape[1]:]
+            for batch_idx in range(len(sequence_tokenized)):
+                for i, tokenized_target_word in enumerate(sequence_tokenized[batch_idx]):
+                    key = selected_last_layer_ffn_input[batch_idx, i, :]
+                    self.dstore.add_item_to_store((key, tokenized_target_word))
         raise NotImplementedError
 
 
@@ -131,10 +146,8 @@ def main():
     # TODO: Load the dataset here
     pass
 
-    # TODO: Create the kNN with datastore
+    # Create the kNN with datastore and train
     knn_lm = kNN_LM(model_name="gpt2-small")
-
-    # TODO: Train the LM
     knn_lm.train()
 
 
