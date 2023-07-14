4414dc381d7972cf5db066c0dc4bc8db259d4f06
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2023-07-14 13:17:49 +0000

    Newscrawl download updated

--- examples/language_model/prepare_newscrawl_20h1.sh
+++ examples/language_model/prepare_newscrawl_20h1.sh
@@ -1,8 +1,34 @@
 !#/bin/bash
 
-echo "Downloading file..."
-wget https://data.statmt.org/news-crawl/en/news.2020.en.shuffled.deduped.gz
+URLS=(
+    "https://data.statmt.org/news-crawl/en/news.2020.en.shuffled.deduped.gz"
+)
+FILES=(
+    "news.2020.en.shuffled.deduped.gz"
+)
 
-echo "Unziping file..."
-gunzip news.2020.en.shuffled.deduped.gz
-
+for ((i=0;i<${#URLS[@]};++i)); do
+    file=${FILES[i]}
+    if [ -f $file ]; then
+        echo "$file already exists, skipping download"
+    else
+        url=${URLS[i]}
+        wget "$url"
+        if [ -f $file ]; then
+            echo "$url successfully downloaded."
+        else
+            echo "$url not successfully downloaded."
+            exit -1
+        fi
+        if [ ${file: -4} == ".tgz" ]; then
+            tar zxvf $file
+        elif [ ${file: -4} == ".tar" ]; then
+            tar xvf $file
+        elif [ ${file: -4} == ".zip" ]; then
+            unzip $file
+        elif [ ${file: -4} == ".gz" ]; then
+            gunzip $file
+        fi
+    fi
+done
+cd ..
