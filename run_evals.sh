a2f77b2887e4deb1093baf6107af4cce68335f56
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2023-07-24 12:03:09 +0000

    prune threshold updated for probability instead of perplexity

--- run_evals.sh
+++ run_evals.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# All experiments use probability as memory strength by default unless `--use-perplexity-mem-strength` flag is used
+
 echo "Generating datastore from the training set (with dataset shuffling)"
 python eval_lm.py data-bin/wikitext-103 \
     --path checkpoints/checkpoint_best.pt \
@@ -8,7 +10,7 @@ python eval_lm.py data-bin/wikitext-103 \
     --context-window 1536 --tokens-per-sample 1536 \
     --dstore-mmap checkpoints/dstore_adaptive_train.pt --knn-keytype 'last_ffn_input' \
     --use-adaptive-mem --model-overrides "{'knn_keytype': 'last_ffn_input'}" \
-    --adaptive-mem-log-prob-thresh -1.0 --prune-memory-strength-thresh 0.1 \
+    --adaptive-mem-log-prob-thresh -1.0 --prune-memory-strength-thresh 0.001 \
     --memory-decay-factor 0.9 --k 1024 --lmbda 0.25 --probe 32 --shuffle-dataset \
     --datastore-update-freq 1 --save-knnlm-dstore --knnlm --fp16 | tee logs/train_datastore.log
 
@@ -19,7 +21,7 @@ python eval_lm.py data-bin/wikitext-103 \
     --context-window 2560 --softmax-batch 1024 --gen-subset valid \
     --dstore-mmap checkpoints/dstore_adaptive_train_val.pt --knn-keytype 'last_ffn_input' \
     --use-adaptive-mem --model-overrides "{'knn_keytype': 'last_ffn_input'}" \
-    --adaptive-mem-log-prob-thresh -1.0 --prune-memory-strength-thresh 0.1 \
+    --adaptive-mem-log-prob-thresh -1.0 --prune-memory-strength-thresh 0.001 \
     --memory-decay-factor 0.9 --k 1024 --lmbda 0.25 --probe 32 \
     --datastore-update-freq 1 --save-knnlm-dstore --knnlm --fp16 \
     --existing_datastore_path checkpoints/dstore_adaptive_train.pt > logs/train_val_datastore.log 2>&1
@@ -31,6 +33,6 @@ python eval_lm.py data-bin/wikitext-103 \
     --context-window 2560 --softmax-batch 1024 --gen-subset valid \
     --dstore-mmap checkpoints/dstore_adaptive_val.pt --knn-keytype 'last_ffn_input' \
     --use-adaptive-mem --model-overrides "{'knn_keytype': 'last_ffn_input'}" \
-    --adaptive-mem-log-prob-thresh -1.0 --prune-memory-strength-thresh 0.1 \
+    --adaptive-mem-log-prob-thresh -1.0 --prune-memory-strength-thresh 0.001 \
     --memory-decay-factor 0.9 --k 1024 --lmbda 0.25 --probe 32 \
     --datastore-update-freq 1 --save-knnlm-dstore --knnlm --fp16 > logs/val_datastore.log 2>&1
