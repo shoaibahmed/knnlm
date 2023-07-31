ead8fa4eb5bb2d142ff251e540907ee52f9cfec1
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2023-07-31 21:12:10 +0200

    New evals added

--- run_evals.sh
+++ run_evals.sh
@@ -26,6 +26,18 @@ python eval_lm.py data-bin/wikitext-103 \
     --datastore-update-freq 1 --save-knnlm-dstore --knnlm --fp16 \
     --existing-datastore-path checkpoints/dstore_adaptive_train.pt > logs/train_val_datastore.log 2>&1
 
+echo "Using a dynamic pretrained (on the training set) datastore on the validation set with adaptive lambda"
+python eval_lm.py data-bin/wikitext-103 \
+    --path checkpoints/checkpoint_best.pt \
+    --sample-break-mode complete --max-tokens 2560 --tokens-per-sample 2560 \
+    --context-window 2560 --softmax-batch 1024 --gen-subset valid \
+    --dstore-mmap checkpoints/dstore_adaptive_train_val.pt --knn-keytype 'last_ffn_input' \
+    --use-adaptive-mem --model-overrides "{'knn_keytype': 'last_ffn_input'}" \
+    --adaptive-mem-log-prob-thresh -1.0 --prune-memory-strength-thresh 0.001 \
+    --memory-decay-factor 0.9 --k 1024 --lmbda 0.25 --probe 32 \
+    --datastore-update-freq 1 --save-knnlm-dstore --knnlm --fp16 --use-adaptive-lmbda \
+    --existing-datastore-path checkpoints/dstore_adaptive_train.pt > logs/train_val_datastore_adaptive_lmbda.log 2>&1
+
 echo "Using a dynamic datastore on the validation set"
 python eval_lm.py data-bin/wikitext-103 \
     --path checkpoints/checkpoint_best.pt \
@@ -36,3 +48,14 @@ python eval_lm.py data-bin/wikitext-103 \
     --adaptive-mem-log-prob-thresh -1.0 --prune-memory-strength-thresh 0.001 \
     --memory-decay-factor 0.9 --k 1024 --lmbda 0.25 --probe 32 \
     --datastore-update-freq 1 --save-knnlm-dstore --knnlm --fp16 > logs/val_datastore.log 2>&1
+
+echo "Using a dynamic datastore on the validation set with adaptive lambda"
+python eval_lm.py data-bin/wikitext-103 \
+    --path checkpoints/checkpoint_best.pt \
+    --sample-break-mode complete --max-tokens 2560 --tokens-per-sample 2560 \
+    --context-window 2560 --softmax-batch 1024 --gen-subset valid \
+    --dstore-mmap checkpoints/dstore_adaptive_val.pt --knn-keytype 'last_ffn_input' \
+    --use-adaptive-mem --model-overrides "{'knn_keytype': 'last_ffn_input'}" \
+    --adaptive-mem-log-prob-thresh -1.0 --prune-memory-strength-thresh 0.001 \
+    --memory-decay-factor 0.9 --k 1024 --lmbda 0.25 --probe 32 --use-adaptive-lmbda \
+    --datastore-update-freq 1 --save-knnlm-dstore --knnlm --fp16 > logs/val_datastore_adaptive_lmbda.log 2>&1
