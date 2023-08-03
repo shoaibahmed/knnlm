e59faf83c1475f86f7561a14bf54c31a1bf85132
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2023-08-03 22:44:46 +0200

    Shuffled datastore support added

--- run_evals.sh
+++ run_evals.sh
@@ -5,24 +5,25 @@ python eval_lm.py data-bin/wikitext-103 \
     --path checkpoints/checkpoint_best.pt \
     --sample-break-mode complete --max-tokens 2560 --tokens-per-sample 2560 \
     --context-window 2560 --softmax-batch 1024 --gen-subset valid \
-    --dstore-mmap checkpoints/dstore_adaptive_train_val.pt --knn-keytype 'last_ffn_input' \
+    --dstore-mmap checkpoints/dstore_adaptive_train_shuffled_val.pt --knn-keytype 'last_ffn_input' \
     --use-adaptive-mem --model-overrides "{'knn_keytype': 'last_ffn_input'}" \
     --adaptive-mem-log-prob-thresh -1.0 --prune-memory-strength-thresh 0.001 \
     --memory-decay-factor 0.9 --k 1024 --lmbda 0.25 --probe 32 \
-    --datastore-update-freq 1 --save-knnlm-dstore --knnlm --fp16 \
-    --existing-datastore-path checkpoints/dstore_adaptive_train.pt > logs/train_val_datastore.log 2>&1
+    --datastore-update-freq 1 --save-knnlm-dstore --knnlm --fp16 --freeze-loaded-memories \
+    --existing-datastore-path checkpoints/dstore_adaptive_train_shuffled.pt > logs/train_shuffled_val_datastore.log 2>&1
 
 echo "Using a dynamic pretrained (on the training set) datastore on the validation set with adaptive lambda"
 python eval_lm.py data-bin/wikitext-103 \
     --path checkpoints/checkpoint_best.pt \
     --sample-break-mode complete --max-tokens 2560 --tokens-per-sample 2560 \
     --context-window 2560 --softmax-batch 1024 --gen-subset valid \
-    --dstore-mmap checkpoints/dstore_adaptive_train_val.pt --knn-keytype 'last_ffn_input' \
+    --dstore-mmap checkpoints/dstore_adaptive_train_shuffled_val.pt --knn-keytype 'last_ffn_input' \
     --use-adaptive-mem --model-overrides "{'knn_keytype': 'last_ffn_input'}" \
     --adaptive-mem-log-prob-thresh -1.0 --prune-memory-strength-thresh 0.001 \
     --memory-decay-factor 0.9 --k 1024 --lmbda 0.25 --probe 32 \
-    --datastore-update-freq 1 --save-knnlm-dstore --knnlm --fp16 --use-adaptive-lmbda \
-    --existing-datastore-path checkpoints/dstore_adaptive_train.pt > logs/train_val_datastore_adaptive_lmbda.log 2>&1
+    --datastore-update-freq 1 --save-knnlm-dstore --knnlm --fp16 \
+    --freeze-loaded-memories --use-adaptive-lmbda \
+    --existing-datastore-path checkpoints/dstore_adaptive_train_shuffled.pt > logs/train_shuffled_val_datastore_adaptive_lmbda.log 2>&1
 
 echo "Using a dynamic datastore on the validation set"
 python eval_lm.py data-bin/wikitext-103 \
@@ -32,7 +33,7 @@ python eval_lm.py data-bin/wikitext-103 \
     --dstore-mmap checkpoints/dstore_adaptive_val.pt --knn-keytype 'last_ffn_input' \
     --use-adaptive-mem --model-overrides "{'knn_keytype': 'last_ffn_input'}" \
     --adaptive-mem-log-prob-thresh -1.0 --prune-memory-strength-thresh 0.001 \
-    --memory-decay-factor 0.9 --k 1024 --lmbda 0.25 --probe 32 \
+    --memory-decay-factor 0.9 --k 1024 --lmbda 0.25 --probe 32 --freeze-loaded-memories \
     --datastore-update-freq 1 --save-knnlm-dstore --knnlm --fp16 > logs/val_datastore.log 2>&1
 
 echo "Using a dynamic datastore on the validation set with adaptive lambda"
@@ -43,5 +44,6 @@ python eval_lm.py data-bin/wikitext-103 \
     --dstore-mmap checkpoints/dstore_adaptive_val.pt --knn-keytype 'last_ffn_input' \
     --use-adaptive-mem --model-overrides "{'knn_keytype': 'last_ffn_input'}" \
     --adaptive-mem-log-prob-thresh -1.0 --prune-memory-strength-thresh 0.001 \
-    --memory-decay-factor 0.9 --k 1024 --lmbda 0.25 --probe 32 --use-adaptive-lmbda \
+    --memory-decay-factor 0.9 --k 1024 --lmbda 0.25 --probe 32 \
+    --freeze-loaded-memories --use-adaptive-lmbda \
     --datastore-update-freq 1 --save-knnlm-dstore --knnlm --fp16 > logs/val_datastore_adaptive_lmbda.log 2>&1
